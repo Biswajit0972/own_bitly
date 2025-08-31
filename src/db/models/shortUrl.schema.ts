7dfcc4a53c91deb784c2ac9994efe4d0149ab68a
Biswajit Das <workbiswajit0981@gmail.com>
2025-08-31 19:52:29 +0530

    Update URL schema field lengths for short_urlID and long_url adjustments ♻️

--- src/db/models/shortUrl.schema.ts
+++ src/db/models/shortUrl.schema.ts
@@ -3,9 +3,8 @@ import {usersTable} from "./user.schema.ts";
 
 export const shortUrlSchema = t.pgTable("short_urls", {
         id: t.integer("id").primaryKey().generatedByDefaultAsIdentity(),
-        short_urlID: t.varchar("short_urlID", {length: 255}).notNull().unique(),
-        long_url: t.varchar("long_url", {length: 255}).notNull(),
-        short_url: t.varchar("short_url", {length: 255}).notNull(),
+        short_urlID: t.varchar("short_urlID", {length: 155}).notNull().unique(),
+        long_url: t.varchar("long_url", {length: 555}).notNull(),
         user_id: t.integer("user_id").references(() => usersTable.id),
         createdAt: t.varchar("created_at", {length: 255}).notNull().default("now()"),
         updatedAt: t.varchar("updated_at", {length: 255}).notNull().default("now()")
