2c0f12e09aec0f509e1a13fa8357a91df2359ce2
Biswajit Das <workbiswajit0981@gmail.com>
2025-08-31 09:19:10 +0530

    Update user schema to add unique constraints and refreshToken field 🚀

--- src/db/models/user.schema.ts
+++ src/db/models/user.schema.ts
@@ -2,10 +2,11 @@ import {integer, pgTable, varchar} from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
     id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    username: varchar("username", {length: 255}).notNull(),
+    username: varchar("username", {length: 255}).notNull().unique(),
     password: varchar("password", {length: 255}).notNull(),
-    email: varchar("email", {length: 255}).notNull(),
+    email: varchar("email", {length: 255}).notNull().unique(),
     fullName: varchar("full_name", {length: 255}).notNull(),
+    refreshToken: varchar("refresh_token", {length: 255}),
     createdAt: varchar("created_at", {length: 255}).notNull().default("now()"),
     updatedAt: varchar("updated_at", {length: 255}).notNull().default("now()")
 });
