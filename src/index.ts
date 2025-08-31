be36e9df61eb73280ce1e31e8776d778f6e0156a
Biswajit Das <workbiswajit0981@gmail.com>
2025-08-31 20:38:07 +0530

    Add user router and integrate it into the Express app ✨

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import "dotenv/config";
 import express from "express";
 import cookieParser from "cookie-parser";
+import userRouter from "./router/user.router.ts";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+
+app.use(userRouter);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
