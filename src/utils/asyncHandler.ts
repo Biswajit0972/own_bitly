032ecf95e08ac493a5bda94302c14dd8d8cd4209
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-02 15:22:56 +0530

    Refactor `asyncHandler` util to support type parameterization ♻️

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -1,26 +1,32 @@
-import {NextFunction, Request, Response} from "express";
-import {AppError} from "./AppError";
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "./AppError";
 
-type AsyncHandler = (req: Request, res: Response, next: NextFunction) => Promise<Response>;
+type AsyncHandler<T = any> = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => Promise<T>;
 
-export const asyncHandler = (fn: AsyncHandler) => {
-    return async (req: Request, res: Response, next: NextFunction): Promise<Response> => {
-
+export const asyncHandler = <T = any>(fn: AsyncHandler<T>) => {
+    return async (req: Request, res: Response, next: NextFunction): Promise<T | Response> => {
         try {
             return await fn(req, res, next);
         } catch (error) {
             if (error instanceof AppError) {
-                console.log(error.message)
-
-                return res.status(error.statusCode || 500).json({message: error.message || "Something went wrong"});
+                console.log(error.message);
+                return res
+                    .status(error.statusCode || 500)
+                    .json({ message: error.message || "Something went wrong" });
             }
 
             if (error instanceof Error) {
-                console.log(error.message)
-                return res.status(500).json({message: error.message || "Something went wrong"});
+                console.log(error.message);
+                return res
+                    .status(500)
+                    .json({ message: error.message || "Something went wrong" });
             }
 
-            return res.status(500).json({message: "unSomething went wrong"});
+            return res.status(500).json({ message: "Unexpected error occurred" });
         }
-    }
-}
+    };
+};
