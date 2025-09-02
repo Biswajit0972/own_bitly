38e654208092ae5dd51ed97cc9cd024d608a477e
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-02 14:57:19 +0530

    Refactor URL controllers to use `shortCode` instead of `id` for consistency ♻️

--- src/utils/Types/types.ts
+++ src/utils/Types/types.ts
@@ -1,8 +1,9 @@
 import * as z from "zod";
 
-import {loginSchema, shortenSchema, userSchema} from "../zod/zodTypes.ts";
+import {loginSchema, shortenSchema, urlSchema, userSchema} from "../zod/zodTypes.ts";
 import {shortUrlSchema} from "../../db/models/shortUrl.schema.ts";
 
 export type User = z.infer<typeof userSchema>;
 export type UserLogin = z.infer<typeof loginSchema>;
 export type UrlBody= z.infer<typeof shortenSchema>;
+export type ShortCode = z.infer<typeof urlSchema>;
