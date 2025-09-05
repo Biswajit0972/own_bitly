3c50f3acc74ca573da107f4a7e6b5c62f205d30a
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-05 22:07:28 +0530

    Update URL router to replace `getUrl` with `redirect` controller ♻️

--- src/router/url.route.ts
+++ src/router/url.route.ts
@@ -4,14 +4,14 @@ import {validate, validateParams} from "../middleware/validation.ts";
 import {shortenSchema, urlSchema} from "../utils/zod/zodTypes.ts";
 import {urlShort} from "../controller/URL/urls.ts";
 import {updateUrl} from "../controller/URL/updateUrl.ts";
-import {getUrl} from "../controller/URL/getUrl.ts";
+import {redirect} from "../controller/URL/redirect.ts";
 import {getAllUrls} from "../controller/URL/getAll.ts";
 import {deleteUrl} from "../controller/URL/deleteUrl.ts";
 
 const urlRouter = Router();
 
 urlRouter.post("/api/url/short", authenticate, validate(shortenSchema), urlShort);  // ✅
-urlRouter.get("/api/url/:shortCode", validateParams(urlSchema), getUrl); // ✅
+urlRouter.get("/api/url/:shortCode", validateParams(urlSchema), redirect); // ✅
 urlRouter.put("/api/urls/update/:shortCode", authenticate, validateParams(urlSchema), updateUrl); // ✅
 urlRouter.delete("/api/urls/delete/:shortCode", authenticate, validateParams(urlSchema), deleteUrl); // ✅
 urlRouter.get("/api/urls/all", authenticate,  getAllUrls); // ✅
