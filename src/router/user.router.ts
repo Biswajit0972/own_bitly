d5c0f435bc801470c31164b4893c788a04a1b048
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-15 12:11:22 +0530

    Update user router with token revalidation endpoint ✨

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import {sign} from "../controller/user/sign.ts";
-import {login} from "../controller/user/login.ts";
-import {userProfile} from "../controller/user/userProfile.ts";
-import {authenticate} from "../middleware/Authentication.ts";
-import {validate} from "../middleware/validation.ts";
-import {loginSchema, userSchema} from "../utils/zod/zodTypes.ts";
+import { sign } from "../controller/user/sign.ts";
+import { login } from "../controller/user/login.ts";
+import { userProfile } from "../controller/user/userProfile.ts";
+import { authenticate, revalidateTokenAuth } from "../middleware/Authentication.ts";
+import { validate } from "../middleware/validation.ts";
+import { loginSchema, userSchema } from "../utils/zod/zodTypes.ts";
+import { updateAccessToken } from "../controller/user/revalidateToken.ts";
 
 const userRouter = Router();
 
@@ -12,5 +13,6 @@ userRouter.post("/api/users/register", validate(userSchema), sign);
 userRouter.post("/api/users/login", validate(loginSchema), login);
 
 userRouter.get("/api/users/getId", authenticate, userProfile);
+userRouter.get("/api/users/revalidate", revalidateTokenAuth, updateAccessToken);
 
 export default userRouter;
