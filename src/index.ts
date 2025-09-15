bcb054073317f80036de88a4453c311dd2f0c6a6
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-15 12:11:07 +0530

    Add CORS middleware for cross-origin requests from client ✨

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import "dotenv/config";
 import express from "express";
+import  cors from  "cors";
 import cookieParser from "cookie-parser";
 import userRouter from "./router/user.router.ts";
 import urlRoute from "./router/url.route.ts";
-
 const app = express();
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
 app.use(userRouter);
-app.use(urlRoute)
+app.use(urlRoute);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
