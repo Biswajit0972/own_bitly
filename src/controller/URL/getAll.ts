cf92ce14aa25c06fcbe3d90ebcedc29c27489ccf
Biswajit Das <workbiswajit0981@gmail.com>
2025-09-18 11:18:53 +0530

    Enhance `getAllUrls` controller to include click count and user details in the response

--- src/controller/URL/getAll.ts
+++ src/controller/URL/getAll.ts
@@ -1,11 +1,11 @@
 import {Request, Response} from "express";
-import {eq} from "drizzle-orm";
+import {count, eq} from "drizzle-orm";
 import {asyncHandler} from "../../utils/asyncHandler.ts";
 import {AppError} from "../../utils/AppError.ts";
 import db from "../../db/databaseConnection.ts";
 import {shortUrlSchema} from "../../db/models/shortUrl.schema.ts";
 import {AppResponse} from "../../utils/AppResponse.ts";
-
+import {clicks_on_short_urlsSchema} from "../../db/models/clicks_on_short_urls.schema.ts";
 
 
 export const getAllUrls = asyncHandler(async (req: Request, res: Response) => {
@@ -14,13 +14,33 @@ export const getAllUrls = asyncHandler(async (req: Request, res: Response) => {
     }
 
     const result = await db
-        .select()
+        .select({
+            id: shortUrlSchema.id,
+            shortUrlID: shortUrlSchema.short_urlID,
+            longUrl: shortUrlSchema.long_url,
+            title: shortUrlSchema.tittle,
+            clicksCount: count(clicks_on_short_urlsSchema.id),
+            createdAt: shortUrlSchema.createdAt,
+            user_id: shortUrlSchema.user_id
+        })
         .from(shortUrlSchema)
-        .where(eq(shortUrlSchema.user_id, req.user?.id));
+        .leftJoin(
+            clicks_on_short_urlsSchema,
+            eq(clicks_on_short_urlsSchema.short_url_id, shortUrlSchema.short_urlID)
+        )
+        .where(eq(shortUrlSchema.user_id, req.user?.id))
+        .groupBy(
+            shortUrlSchema.id,
+            shortUrlSchema.short_urlID,
+            shortUrlSchema.long_url,
+            shortUrlSchema.tittle
+        );
 
     if (result.length === 0) {
         throw new AppError(404, "Short URL not found");
     }
+
+    console.log(result);
 
     return res
         .status(200)
