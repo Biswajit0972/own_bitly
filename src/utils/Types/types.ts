ef0dfbd7b2c9c2ef8aff03b8498dd4a5a9cee5f4
Biswajit Das <workbiswajit0981@gmail.com>
2025-11-22 09:01:20 +0530

    Add `Authentication` interface to `types.ts` for token management and request authentication

--- src/utils/Types/types.ts
+++ src/utils/Types/types.ts
@@ -1,8 +1,18 @@
 import * as z from "zod";
 
 import {loginSchema, shortenSchema, urlSchema, userSchema} from "../zod/zodTypes.ts";
+import {JwtPayload} from "jsonwebtoken";
+import {Request, Response,NextFunction} from "express";
 
 export type User = z.infer<typeof userSchema>;
 export type UserLogin = z.infer<typeof loginSchema>;
 export type UrlBody= z.infer<typeof shortenSchema>;
 export type ShortCode = z.infer<typeof urlSchema>;
+
+export interface Authentication {
+    getJwtSecret: () => string;
+    extractTokenFromHeader: (req: Request) => string | undefined | null;
+    verifyToken: (req: Request) => JwtPayload | null;
+    authenticateRequest: (req: Request, res: Response, next: NextFunction) => Response | NextFunction | void;
+    revalidateToken: (req: Request, res: Response, next: NextFunction) => Response | NextFunction | void;
+}
