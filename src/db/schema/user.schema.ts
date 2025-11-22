90729a4fb33b76778d30b312f29f4b2c835660f7
Biswajit Das <workbiswajit0981@gmail.com>
2025-11-22 08:11:48 +0530

    Update `user.schema.ts` to adjust column lengths and fix field naming inconsistency

--- src/db/schema/user.schema.ts
+++ src/db/schema/user.schema.ts
@@ -2,10 +2,10 @@ import {integer, pgTable, varchar} from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
     id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    username: varchar("username", {length: 255}).notNull().unique(),
+    username: varchar("username", {length: 55}).notNull().unique(),
     password: varchar("password", {length: 255}).notNull(),
-    email: varchar("email", {length: 255}).notNull().unique(),
-    fullName: varchar("full_name", {length: 255}).notNull(),
+    email: varchar("email", {length: 100}).notNull().unique(),
+    fullName: varchar("fullName", {length: 255}).notNull(),
     refreshToken: varchar("refresh_token", {length: 255}),
     createdAt: varchar("created_at", {length: 255}).notNull().default(new Date().toISOString()),
     updatedAt: varchar("updated_at", {length: 255}).notNull().default(new Date().toISOString())
