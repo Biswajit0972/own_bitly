d34a4545b1d55ed003419d0011b7c930881cb73d
Biswajit Das <workbiswajit0981@gmail.com>
2025-11-22 08:19:16 +0530

    Refactor `clicks_on_short_urls.schema.ts` to fix field naming inconsistency, add `browser` column, and update reference for `shortUrlId` field

--- src/db/schema/clicks_on_short_urls.schema.ts
+++ src/db/schema/clicks_on_short_urls.schema.ts
@@ -4,9 +4,10 @@ import {shortUrlSchema} from "./shortUrl.schema.ts";
 export const clicks_on_short_urlsSchema = t.pgTable("clicks_on_short_urls", {
     id: t.integer("id").primaryKey().generatedByDefaultAsIdentity(),
     click_time: t.timestamp().defaultNow().notNull(),
-    short_url_id: t.varchar("short_url_id").references(() => shortUrlSchema.short_urlID, { onDelete: "cascade" }),
+    shortUrlId: t.varchar("shortUrlId").references(() => shortUrlSchema.id, { onDelete: "cascade" }),
     user_agent: t.varchar("user_agent", {length: 255}).notNull(),
     ip_address: t.varchar("ip_address", {length: 255}).notNull(),
+    browser: t.varchar("browser", {length: 255}).notNull(),
     referer: t.varchar("referer", {length: 255}).notNull(),
     createdAt: t.varchar("created_at", {length: 255}).notNull().default(new Date().toISOString()),
     updatedAt: t.varchar("updated_at", {length: 255}).notNull().default(new Date().toISOString())
