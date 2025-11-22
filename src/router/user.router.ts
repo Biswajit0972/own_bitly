7770bdb973d51893126a2e1a58773f9fd59c899e
Biswajit Das <workbiswajit0981@gmail.com>
2025-11-22 10:02:10 +0530

    Refactor `user.router.ts` to use `AuthenticationHelper` class and update method references for authentication and token revalidation

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -1,18 +1,21 @@
-import { Router } from "express";
-import { sign } from "../controller/user/sign.ts";
-import { login } from "../controller/user/login.ts";
-import { userProfile } from "../controller/user/userProfile.ts";
-import { authenticate, revalidateTokenAuth } from "../middleware/Authentication.ts";
-import { validate } from "../middleware/validation.ts";
-import { loginSchema, userSchema } from "../utils/zod/zodTypes.ts";
-import { updateAccessToken } from "../controller/user/revalidateToken.ts";
+import {Router} from "express";
+import {sign} from "../controller/user/sign.ts";
+import {login} from "../controller/user/login.ts";
+import {userProfile} from "../controller/user/userProfile.ts";
+import {AuthenticationHelper} from "../middleware/Authentication.ts";
+import {validate} from "../middleware/validation.ts";
+import {loginSchema, userSchema} from "../utils/zod/zodTypes.ts";
+import {updateAccessToken} from "../controller/user/revalidateToken.ts";
 
 const userRouter = Router();
+const authHelper = new AuthenticationHelper();
 
 userRouter.post("/api/users/register", validate(userSchema), sign);
 userRouter.post("/api/users/login", validate(loginSchema), login);
 
-userRouter.get("/api/users/getId", authenticate, userProfile);
-userRouter.get("/api/users/revalidate", revalidateTokenAuth, updateAccessToken);
+//! user profile
+userRouter.get("/api/users/getId", authHelper.authenticateRequest, userProfile);
+// ! revalidate token
+userRouter.get("/api/users/revalidate", authHelper.revalidateToken, updateAccessToken);
 
 export default userRouter;
